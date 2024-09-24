775a6870a001353ab28dc35a00307510621578d2
josue baez <josuebaez61@gmail.com>
2024-09-23 22:38:02 -0300

    ADD, custom modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListaAlumnosComponent } from './components/lista-alumnos/lista-alumnos.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { CommonModule } from '@angular/common';
+
+// interface Alumno {
+//   nombre: string;
+// }
+
+// const miAlumno: Alumno = {
+// }
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListaAlumnosComponent],
+  imports: [CommonModule, RouterOutlet, ListaAlumnosComponent, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  modalVisible = false;
+
   title = 'clase-05-comunicacion';
 
   alumnosAprobados = ['Naruto', 'Goku'];
   alumnosDesaprobados = ['Gohan'];
+
+  @ViewChild('myButton') myButtonRef?: ElementRef<HTMLButtonElement>;
+
+  @ViewChild(ListaAlumnosComponent)
+  listaAlumnosComponentRef?: ListaAlumnosComponent;
+
+  constructor() {
+    console.log(this.myButtonRef);
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.myButtonRef);
+    console.log(this.listaAlumnosComponentRef);
+  }
 
   eliminarAlumno(index: number, from: 'aprobados' | 'desaprobados') {
     // this.alumnosAprobados = this.alumnosAprobados.splice(
---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss',
+})
+export class ModalComponent {
+  @Input() visible = false;
+  @Output() visibleChange = new EventEmitter<boolean>();
+
+  @Output() cerrar = new EventEmitter();
+}

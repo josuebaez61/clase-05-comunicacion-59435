453c8ad37ac5c9bcc91493a62e18dace3d7de020
josue baez <josuebaez61@gmail.com>
2024-09-23 21:40:21 -0300

    Se agrego lista de alumnos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaAlumnosComponent } from './components/lista-alumnos/lista-alumnos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaAlumnosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'clase-05-comunicacion';
+
+  alumnosAprobados = ['Naruto', 'Goku'];
+  alumnosDesaprobados = ['Gohan'];
+
+  eliminarAlumno(index: number, from: 'aprobados' | 'desaprobados') {
+    // this.alumnosAprobados = this.alumnosAprobados.splice(
+    //   index === 0 ? index : index - 1,
+    //   1
+    // );
+    if (from === 'aprobados') {
+      this.alumnosAprobados = this.alumnosAprobados.filter(
+        (_, i) => index != i
+      );
+    } else {
+      this.alumnosDesaprobados = this.alumnosDesaprobados.filter(
+        (_, i) => index != i
+      );
+    }
+  }
 }
---
+++ src/app/components/lista-alumnos/lista-alumnos.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-alumnos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './lista-alumnos.component.html',
+  styleUrl: './lista-alumnos.component.scss',
+})
+export class ListaAlumnosComponent {
+  /**
+   * El decorador @Input nos sirve para indicar que esta propiedad
+   * la vamos a recibir desde el padre
+   */
+  @Input() alumnos: string[] = [];
+
+  /**
+   * El decorador @Ouput nos sirve para notificar al padre de que algo
+   * sucedio
+   */
+  @Output() eliminarAlumno = new EventEmitter<number>();
+}
